93c0e99826e6470b68cea17b076df87356a3c93b
Ruhan-Saad-Dave <149294639+Ruhan-Saad-Dave@users.noreply.github.com>
2024-03-23 22:32:27 +0530

    added high low number guessing game

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+import random
+answer = random.randint(0,100)
+ans = -1
+tries = 0
+
+while ans != answer:
+    ans = input("Enter your guess:")
+    ans = int(ans)
+    if ans < answer:
+        print("Answer is higher")
+    elif ans > answer:
+        print("Answer is lower")
+    tries += 1
+else:
+    print("You win, the answer is:", answer,)
+    print("You found it in",tries,"tries")
