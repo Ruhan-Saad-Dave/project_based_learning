3b67360f87267b9643027d01d43a79cf96299f1c
Ruhan-Saad-Dave <149294639+Ruhan-Saad-Dave@users.noreply.github.com>
2024-03-24 20:11:15 +0530

    update intro page
    made the back.py empty and updated the gui in mai.py

--- pbl_app/main.py
+++ pbl_app/main.py
@@ -10,72 +10,142 @@ from kivy.graphics import Color, Rectangle
 class MainPage(Screen):
     def __init__(self,**kwargs):
         super(MainPage, self).__init__(**kwargs)
-        button = Button(text = "hit me", on_press = self.next)
+        button = Button(text = "Welcome to main page\nOops, you are a bit early!\nWork under progress", on_press = self.start)
         self.add_widget(button)
 
-    def next(self,instance):
-        self.manager.current = "toolpage"
+    def start(self, instance):
+        self.manager.transition = SlideTransition(direction = "down")
+        self.manager.current = "start1page"
 
-class StartPage(Screen):
+class Start1Page(Screen):
     def __init__(self,**kwargs):
-        super(StartPage,self).__init__(**kwargs)
-
-        page = PageLayout()
+        super(Start1Page,self).__init__(**kwargs)
         pg1 = BoxLayout(orientation = "vertical")
-        pg2 = BoxLayout(orientation = "vertical")
-        pg3 = BoxLayout(orientation = "vertical")
-        pg4 = BoxLayout(orientation = "vertical")
-
-
         top1_label = Label(text = "Welcome to", size_hint = (1.0, 0.25), font_size = 50 ,color=(1, 1, 1, 1))
-        logo1_label = Label(text = "#Place holder for project logo",size_hint = (1.0, 0.25))
+        logo1_label = Label(text = "#1Place holder for project logo",size_hint = (1.0, 0.25))
         text1_label = Label(text = "Before we begin, lets see \nall the features you \ncould get your hands on",size_hint = (1.0, 0.25), font_size = 20)
-        blank1_label = Label(text = "(Slide left to continue)", size_hint = (1.0, 0.25))
+ 
+        pg1_1 = BoxLayout(size_hint = (1.0, 0.2))
+        btn1_skip = Button(text = "Skip", size_hint = (0.3, 1.0), on_press = self.skip)
+        btn1_next = Button(text = "Next", size_hint = (0.3, 1.0), on_press = self.next)
+        pg1_1.add_widget(btn1_skip)
+        pg1_1.add_widget(btn1_next)
+
         pg1.add_widget(top1_label)
         pg1.add_widget(logo1_label)
         pg1.add_widget(text1_label)
-        pg1.add_widget(blank1_label)
+        pg1.add_widget(pg1_1)
 
-        logo2_label = Label(text = "#Placeholder for project logo", size_hint = (1.0, 0.2))
+        self.add_widget(pg1)
+
+    def skip(self, instance):
+        self.manager.transition = SlideTransition(direction = "up")
+        self.manager.current = "mainpage"
+    
+    def next(self, instance):
+        self.manager.transition = SlideTransition()
+        self.manager.current = "start2page"
+
+class Start2Page(Screen):
+    def __init__(self,**kwargs):
+        super(Start2Page,self).__init__(**kwargs)
+        pg2 = BoxLayout(orientation = "vertical")
+        logo2_label = Label(text = "#2Placeholder for project logo", size_hint = (1.0, 0.2))
         my_text1 = "In CRoom, you can: \n-Find 1k+ hostels and flats in you area \n-Search for hostel/flat in 20+ areas \n-Save your favourite room for later \n-be a landlord \n-And more..."
         text2_label = Label(text = my_text1)
+
+        pg2_1 = BoxLayout(size_hint = (1.0, 0.2))
+        btn2_skip = Button(text = "Skip", size_hint = (0.3, 1.0), on_press = self.skip)
+        btn2_pre = Button(text = "Previous", size_hint = (0.3, 1.0), on_press = self.pre)
+        btn2_next = Button(text = "Next", size_hint = (0.3, 1.0), on_press = self.next)
+        
+        pg2_1.add_widget(btn2_skip)
+        pg2_1.add_widget(btn2_pre)
+        pg2_1.add_widget(btn2_next)
+
         pg2.add_widget(logo2_label)
         pg2.add_widget(text2_label)
+        pg2.add_widget(pg2_1)
+        self.add_widget(pg2)
 
-        logo3_label = Label(text = "#Placeholder for project logo", size_hint = (1.0, 0.2))
+    def skip(self, instance):
+        self.manager.transition = SlideTransition(direction = "up")
+        self.manager.current = "mainpage"
+
+    def pre(self, instance):
+        self.manager.transition = SlideTransition(direction = "right")
+        self.manager.current = "start1page"
+    
+    def next(self, instance):
+        self.manager.transition = SlideTransition()
+        self.manager.current = "start3page"
+
+class Start3Page(Screen):
+    def __init__(self,**kwargs):
+        super(Start3Page,self).__init__(**kwargs)
+        pg3 = BoxLayout(orientation = "vertical")
+        logo3_label = Label(text = "#3Placeholder for project logo", size_hint = (1.0, 0.2))
         my_text2 = "We also provide:\n  Zer0 paper work,\n  Zer0 brokerage,\n  Zer0 Installment,\n  Reasonable price for call and subscription!"
         text3_label = Label(text = my_text2)
+
+        pg3_1 = BoxLayout(size_hint = (1.0, 0.2))
+        btn3_skip = Button(text = "Skip", size_hint = (0.3, 1.0), on_press = self.skip)
+        btn3_pre = Button(text = "Previous", size_hint = (0.3, 1.0), on_press = self.pre)
+        btn3_next = Button(text = "Next", size_hint = (0.3, 1.0), on_press = self.next)
+        pg3_1.add_widget(btn3_skip)
+        pg3_1.add_widget(btn3_pre)
+        pg3_1.add_widget(btn3_next)
+
+
         pg3.add_widget(logo3_label)
         pg3.add_widget(text3_label)
+        pg3.add_widget(pg3_1)
+        self.add_widget(pg3)
 
-        logo4_label = Label(text = "#Placeholder for project logo", size_hint = (1.0, 0.2))
+    def skip(self, instance):
+        self.manager.transition = SlideTransition(direction = "up")
+        self.manager.current = "mainpage"
+    
+    def pre(self, instance):
+        self.manager.transition = SlideTransition(direction = "right")
+        self.manager.current = "start2page"
+    
+    def next(self, instance):
+        self.manager.transition = SlideTransition()
+        self.manager.current = "start4page"
+
+class Start4Page(Screen):
+    def __init__(self,**kwargs):
+        super(Start4Page,self).__init__(**kwargs)
+        pg4 = BoxLayout(orientation = "vertical")
+        logo4_label = Label(text = "#4Placeholder for project logo", size_hint = (1.0, 0.2))
         text4_label = Label(text = "So what are you \nwaiting for?", size_hint = (1.0,0.2))
         text4_1_label = Label(text = "Put on your shoes\nand lets get\nstarted", size_hint = (1.0,0.2))
         text4_2_label = Label(text = "#Placeholder for image")
-        button = Button(text = "Start", size_hint = (0.3,0.2), pos_hint = {"right" : 1})
+
+        pg4_1 = BoxLayout(size_hint = (1.0, 0.2))
+        btn4_start = Button(text = "Start", size_hint = (0.3, 1.0), on_press = self.start)
+        btn4_pre = Button(text = "Previous", size_hint = (0.3, 1.0), on_press = self.pre)
+        pg4_1.add_widget(btn4_pre)
+        pg4_1.add_widget(btn4_start)
+        
+
         pg4.add_widget(logo4_label)
         pg4.add_widget(text4_label)
         pg4.add_widget(text4_1_label)
         pg4.add_widget(text4_2_label)
-        pg4.add_widget(button)
+        pg4.add_widget(pg4_1)
+        self.add_widget(pg4)
 
-
-        page.add_widget(pg1)
-        page.add_widget(pg2)
-        page.add_widget(pg3)
-        page.add_widget(pg4)
-
-        self.add_widget(page)
-
-        
-    def gotomain(self,instance):
+    def start(self, instance):
         self.manager.transition = SlideTransition(direction = "up")
         self.manager.current = "mainpage"
+    
+    def pre(self, instance):
+        self.manager.transition = SlideTransition(direction = "right")
+        self.manager.current = "start3page"
 
-    def update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
-
+    
 class ToolPage(Screen):
     def __init__(self,**kwargs):
         super(ToolPage,self).__init__(**kwargs)
@@ -108,7 +178,10 @@ class MyApp(App):
     def build(self):
         screen_manager = ScreenManager()
         
-        screen_manager.add_widget(StartPage(name = "startpage"))
+        screen_manager.add_widget(Start1Page(name = "start1page"))
+        screen_manager.add_widget(Start2Page(name = "start2page"))
+        screen_manager.add_widget(Start3Page(name = "start3page"))
+        screen_manager.add_widget(Start4Page(name = "start4page"))
         screen_manager.add_widget(MainPage(name = "mainpage"))
         screen_manager.add_widget(ToolPage(name = "toolpage"))
         screen_manager.add_widget(MessagePage(name = "messagepage"))
