115224226898d68ba91d5e020b97d318bac96efd
Ruhan-Saad-Dave <149294639+Ruhan-Saad-Dave@users.noreply.github.com>
2024-03-24 15:33:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import random
-answer = random.randint(0,100)
-ans = -1
-tries = 0
+num1 = int(input("Enter 1st number"))
+num2 = int(input("Enter 2nd number"))
+num3 = int(input("Enter 3rd number"))
 
-while ans != answer:
-    ans = input("Enter your guess:")
-    ans = int(ans)
-    if ans < answer:
-        print("Answer is higher")
-    elif ans > answer:
-        print("Answer is lower")
-    tries += 1
-else:
-    print("You win, the answer is:", answer,)
-    print("You found it in",tries,"tries")
+l1 = [num1, num2, num3]
+n = len(l1)
+
+for i in range(0, n-1):
+    for j in range(i+1,n):
+        if l1[i] > l1[j]:
+            max = l1[i]
+        elif l1[i] < l1[j]:
+            max = li[j]
+
+print(max)
