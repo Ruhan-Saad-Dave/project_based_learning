3c81e12e1ebaeceb100d89ffb2c4148d0d664868
Ruhan-Saad-Dave <149294639+Ruhan-Saad-Dave@users.noreply.github.com>
2024-03-25 17:20:27 +0530

    added instructions to back.py

---
+++ pbl_app/back.py
@@ -0,0 +1,7 @@
+"""
+This file is the backend part of the project
+It takes in data from the GUI, process it and return it back to the GUI
+Needs database(local and online) and server
+"""
+
+#underconstruction
